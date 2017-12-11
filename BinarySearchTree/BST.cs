d3959dff6a7517c0f33a069e366c086d036dabd7
RickK213 <rkippert@att.net>
2017-12-11 13:57:13 -0600

    Finsihed Node class

--- BinarySearchTree/BST.cs
+++ BinarySearchTree/BST.cs
@@ -8,5 +8,35 @@ namespace BinarySearchTree
 {
     class BST
     {
+        //member variables
+        Node head = null;
+
+        //constructor
+        public BST()
+        {
+
+        }
+
+        //member methods
+        public void Add(int item)
+        {
+            Node node = new Node(item);
+            if (head == null)
+            {
+                head = node;
+                return;
+            }
+            Node current = head;
+            while (current.left != null)
+            {
+                if (  )
+                {
+
+                }
+                current = current.left; //keep traversing until the the next node is null
+            }
+            current.left = node;
+        }
+
     }
 }
--- BinarySearchTree/Node.cs
+++ BinarySearchTree/Node.cs
@@ -8,5 +8,19 @@ namespace BinarySearchTree
 {
     class Node
     {
+        public int data; //this is public because we're being lazy
+        public Node left;
+        public Node right;
+
+        public Node()
+        {
+
+        }
+
+        public Node(int item)
+        {
+            data = item;
+        }
+
     }
 }
